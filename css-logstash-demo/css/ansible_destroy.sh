14ffce045fad229a9ed654a2c98e9aa117eaf8ff
brederle <bernd.rederlechner@t-systems.com>
2019-07-28 14:00:09 +0000

    Change playbook location

--- css-logstash-demo/css/ansible_destroy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ansible-playbook -v -i inventories/css-logstash-demo/hosts  --vault-password-file ~/.ssh/.ansible_vault_pwd --skip-tags "apply"\
-  --tags "css" \
-  -e "{ 'net_admin_debug': False, 'state': 'absent' }" site.yml 
